5a4da30c1f6b699db3b06d4625025cac33ceeb37
Davey Knific <dknific@gmail.com>
2018-06-25 13:22:46 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
     <h2>Vue is a powerful JavaScript library.</h2>
     <p>Click the links to see some examples of what you can do!</p>
     <nav>
-      <a href="">Alter CSS</a>
-      <a href="/#/DataBindingDemo">Two-way Data Binding</a>
+      <a href="">Alter CSS</a>・
+      <a href="/#/DataBindingDemo">Two-way Data Binding</a>・
       <a href="">Some other one</a>
     </nav>
     <router-view/>

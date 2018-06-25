1e2442ba1c9bb1d58ab8114d018bb4bf5c270773
Davey Knific <dknific@gmail.com>
2018-06-25 13:20:26 -0700

    Update DataBindingDemo.vue

--- src/components/DataBindingDemo.vue
+++ src/components/DataBindingDemo.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="data-binding-demo-component">
-    <input type="text" placeholder="yoyoyoy"/>
+    <h3>Type something into the input box and watch the DOM update instantly.</h3>
+    <input v-model="message" type="text" placeholder="Type something here..."/>
+    <p class="output">{{message}}</p>
+    <p>Vue provides directives like <span>v-model</span> that allow you to bind your DOM elements to data.</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DataBindingDemo'
+  name: 'DataBindingDemo',
+  data () {
+    return {
+      message: 'Change this message!'
+    }
+  }
 }
 </script>
 
 <style scoped>
+input {
+  text-align: center;
+}
 
+.output {
+  margin-top: 20px;
+  font-style: italic;
+}
+
+div p:nth-child(4) {
+  margin-top: 20px;
+}
+
+p span {
+  font-family: Courier;
+  background: #dbdbdb;
+  padding: 0 5px;
+}
 </style>
